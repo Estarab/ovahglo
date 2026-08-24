154bb5ef122c8f0f582719265e31443519a56107
Abraham <abnationtech@gmail.com>
2026-08-24 16:03:39 +0200

    slider mobile

--- src/components/MobileSlider.jsx
+++ src/components/MobileSlider.jsx
@@ -3,12 +3,12 @@ import Slider from 'react-slick';
 
 // Import your media (1 video and 3 images)
 import video1 from '../assets/our work/mobilebanner1.png';  
-import image1 from '../assets/our work/embroidery.mp4'; 
+import image1 from '../assets/our work/embroideryslider.png'; 
 import image2 from '../assets/our work/webdev-mobile.png';
-import image3 from '../assets/our work/embroidery2.mp4';
-import image4 from '../assets/our work/carbranding.mp4';
-import image5 from '../assets/our work/heatprint.mp4';
-import image6 from '../assets/our work/bannerprint.mp4';
+import image3 from '../assets/our work/embroideryslider.png';
+import image4 from '../assets/our work/embroideryslider.png';
+import image5 from '../assets/our work/embroideryslider.png';
+import image6 from '../assets/our work/embroideryslider.png';
 import image7 from '../assets/our work/embrodery2.jpg';
 
 
